fcbf97669e449d48455c46f5a3f8629d24a86224
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-13 23:18:51 -0600

    Searching

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,9 +1,11 @@
 from question_model import Questions
 from data import question_data
+import random
 
 
 question_bank = []
 for data in question_data:
+    question_bank.append(Questions(data["text"], data["answer"]))
 
-    question_bank.append(Questions())
 
+print(random.choice(question_bank["text"]))
