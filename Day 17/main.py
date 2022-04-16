dee606e0be2369dd9d9cef78ace46a2ab28e74d9
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-15 22:34:41 -0600

    Updated words

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,13 +1,15 @@
-from question_model import Questions
+from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
-import random
 
-
-question_bank = []
+question_list = []
 for data in question_data:
-    question = Questions(data["text"], data["answer"])
-    question_bank.append(question.text)
+    new_question = Question(data["question"], data["correct_answer"])
+    question_list.append(new_question)
+quiz = QuizBrain(question_list)
 
-print(random.choice(question_bank))
+while quiz.still_has_q():
+    quiz.question()
 
-
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{len(question_list)}")
--- Day 17/quiz_brain.py
+++ Day 17/quiz_brain.py
@@ -1,4 +1,24 @@
 class QuizBrain:
     def __init__(self, q_list):
-        self.question_number = 0
-        self.question_list = q_list
+        self.q_number = 0
+        self.score = 0
+        self.q_list = q_list
+
+    def still_has_q(self):
+        return self.q_number != len(self.q_list)
+
+    def question(self):
+        q_question = self.q_list[self.q_number]
+        self.q_number += 1
+        u_answer = input(f"Q.{self.q_number}: {q_question.text} (true/false): ")
+        self.check_answer(u_answer, q_question)
+
+    def check_answer(self, u_answer, q_question):
+        if u_answer.lower() == q_question.answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That was incorrect.")
+        print(f"The correct answer was: {q_question.answer}.")
+        print(f"Your current score is: {self.score}/{self.q_number}")
+        print("\n")
