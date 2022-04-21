bc9ac215dc88337d2d0fdeb30f3dcc8a591167ee
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-21 02:01:22 -0600

    Improvements

--- Day 18/final.py
+++ Day 18/final.py
@@ -11,14 +11,13 @@ color_list = [(58, 106, 148), (224, 200, 110), (134, 84, 58), (223, 138, 62), (1
                                                                                                                 230)]
 
 tom.sety(-300)
+tom.setx(-200)
 for x in range(10):
-    tom.setx(0)
+    tom.setx(-200)
     tom.sety(tom.ycor() + 50)
     for y in range(10):
-        tom.down()
         color_choice = random.choice(color_list)
         tom.dot(20, color_choice)
-        tom.up()
         tom.forward(50)
 
 
