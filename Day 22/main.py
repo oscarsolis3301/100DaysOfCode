75656f8952831d57c4085c763304a25614795b27
Oscar Solis <oscarsolis3301@gmail.com>
2022-05-03 23:29:07 -0600

    Start of the day 22

---
+++ Day 22/main.py
@@ -0,0 +1,9 @@
+from enemy import Enemy
+from character import Character
+from ball import Ball
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.exitonclick()
