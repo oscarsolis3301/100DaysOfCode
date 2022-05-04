66a63edae954819e27848777de6fb8570271506b
Oscar Solis <oscarsolis3301@gmail.com>
2022-05-04 01:28:28 -0600

    Uncommenting

--- Day 22/character.py
+++ Day 22/character.py
@@ -10,18 +10,18 @@ class Character(Turtle):
         self.color("white")
         print(f"currently at: {self.ycor()}")
 
-    # def move_up(self):
-    #     if 280 > self.ycor() >= -280:
-    #         self.forward(20)
-    #         print(f"currently at: {self.ycor()}")
-    #
-    #     else:
-    #         pass
-    #
-    # def move_down(self):
-    #     if 280 >= self.ycor() > -280:
-    #         self.backward(20)
-    #         print(f"currently at: {self.ycor()}")
-    #
-    #     else:
-    #         pass
+    def move_up(self):
+        if 280 > self.ycor() >= -280:
+            self.forward(20)
+            print(f"currently at: {self.ycor()}")
+
+        else:
+            pass
+
+    def move_down(self):
+        if 280 >= self.ycor() > -280:
+            self.backward(20)
+            print(f"currently at: {self.ycor()}")
+
+        else:
+            pass
--- Day 22/main.py
+++ Day 22/main.py
@@ -4,13 +4,14 @@ from ball import Ball
 from turtle import Screen
 
 screen = Screen()
-screen.setup(width=60, height=60)
+screen.setup(width=800, height=600)
 main_paddle = Character()
 
 screen.bgcolor("black")
-#
-# screen.onkeypress(main_paddle.move_up, "Up")
-# screen.onkeypress(main_paddle.move_down, "Down")
+
+screen.onkeypress(main_paddle.move_up, "Up")
+screen.onkeypress(main_paddle.move_down, "Down")
+
 screen.listen()
 
 screen.exitonclick()
