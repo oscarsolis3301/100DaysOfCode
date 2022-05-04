55faefba34785f675ea79725a31b02336c809337
Oscar Solis <oscarsolis3301@gmail.com>
2022-05-04 01:27:35 -0600

    Moving up and down within bounds

--- Day 22/character.py
+++ Day 22/character.py
@@ -1,3 +1,27 @@
-class Character:
+from turtle import Turtle
+
+
+class Character(Turtle):
     def __init__(self):
-        pass
+        super().__init__()
+        self.setheading(90)
+        self.up()
+        self.goto(0, 0)
+        self.color("white")
+        print(f"currently at: {self.ycor()}")
+
+    # def move_up(self):
+    #     if 280 > self.ycor() >= -280:
+    #         self.forward(20)
+    #         print(f"currently at: {self.ycor()}")
+    #
+    #     else:
+    #         pass
+    #
+    # def move_down(self):
+    #     if 280 >= self.ycor() > -280:
+    #         self.backward(20)
+    #         print(f"currently at: {self.ycor()}")
+    #
+    #     else:
+    #         pass
