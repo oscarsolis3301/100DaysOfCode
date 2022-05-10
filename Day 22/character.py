093f75a4f5990a9394fc4518f0f1fac03f9a780d
Oscar Solis <oscarsolis3301@gmail.com>
2022-05-09 23:25:19 -0600

    Day 22 - Paddle improvement and bug fixes

--- Day 22/character.py
+++ Day 22/character.py
@@ -1,27 +1,32 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 
 class Character(Turtle):
     def __init__(self):
         super().__init__()
-        self.setheading(90)
+        self.screen = Screen()
+        self.screen.tracer(0)
+        self.speed("fastest")
         self.up()
-        self.goto(0, 0)
+        self.goto(380, 0)
+        self.setheading(90)
+        self.shapesize(stretch_len=5)
+        self.shape("square")
         self.color("white")
         print(f"currently at: {self.ycor()}")
 
     def move_up(self):
-        if 280 > self.ycor() >= -280:
+        if 260 > self.ycor() >= -260:
             self.forward(20)
             print(f"currently at: {self.ycor()}")
-
+            self.screen.update()
         else:
             pass
 
     def move_down(self):
-        if 280 >= self.ycor() > -280:
+        if 260 >= self.ycor() > -260:
             self.backward(20)
             print(f"currently at: {self.ycor()}")
-
+            self.screen.update()
         else:
             pass
--- Day 22/main.py
+++ Day 22/main.py
@@ -5,12 +5,14 @@ from turtle import Screen
 
 screen = Screen()
 screen.setup(width=800, height=600)
+screen.tracer(0)
 main_paddle = Character()
 
 screen.bgcolor("black")
-
+screen.update()
 screen.onkeypress(main_paddle.move_up, "Up")
 screen.onkeypress(main_paddle.move_down, "Down")
+
 
 screen.listen()
 
