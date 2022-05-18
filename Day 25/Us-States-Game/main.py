279c8990445235933b67a0d496087905c1a8f40f
Oscar Solis <oscarsolis3301@gmail.com>
2022-05-18 00:12:14 -0600

    Day 25 finish

--- Day 25/Us-States-Game/main.py
+++ Day 25/Us-States-Game/main.py
@@ -10,14 +10,15 @@ turtle = Turtle()
 turtle.shape(image)
 
 states_file = pandas.read_csv("50_states.csv")
-states = states_file.state
+states = states_file.state.to_list()
 
 amount_correct = 0
 correct_guesses = []
 playing = True
 while playing:
-
     user_guess = screen.textinput(prompt="What's another state name?", title=f"{amount_correct}/50 States Correct")
+    if user_guess.lower() == "exit":
+        break
     for i in states:
         if user_guess.title() == i and user_guess.title() not in correct_guesses:
             amount_correct += 1
@@ -31,4 +32,14 @@ while playing:
             state.write(user_guess.title())
         elif user_guess.title() in correct_guesses:
             pass
-screen.mainloop()
+
+did_not_guess = ["States Not Guessed"]
+for state in states:
+    if state not in correct_guesses:
+        did_not_guess.append(state)
+
+print(did_not_guess)
+
+db = pandas.Series(did_not_guess)
+db.to_csv("States_to_Study.csv", index=False, header=False)
+
