8a577162a979afc98c7e497dde6b55c043bf6ab7
Oscar Solis <oscarsolis3301@gmail.com>
2022-05-30 04:26:42 -0600

    Tkinter Grid System

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,8 +1,24 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
+window.minsize(width=500, height=500)
 
-button = tkinter.Button()
-button.pack()
+label = Label(text="New Text")
+label.grid(column=0, row=0)
+
+
+def button_clicked():
+    text = entry.get()
+    label.config(text=f"{text}")
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+entry = Entry(width=10)
+entry.grid(column=3, row=3)
 
 window.mainloop()
