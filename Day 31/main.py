510fad03386ffa0a980dea9844f7fd1686b9fef2
oscarsolis3301 <oscarsolis3301@gmail.com>
2022-07-26 14:09:31 -0600

    Finishing touches

--- Day 31/main.py
+++ Day 31/main.py
@@ -7,7 +7,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-French, clicked = True, True
+currently_french = None
 set_word = ''
 timer = None
 words_unknown = {}
@@ -17,71 +17,63 @@ def restart_counter():
     window.after_cancel(timer)
 
 
-def french():
-    global French, set_word
-    French = False
-    canvas.itemconfig(word, text=set_word)
-    canvas.itemconfig(language, text='French')
+def change_to_french():
+    global currently_french, set_word
+    set_word = random.choice(list(words_unknown.keys()))
+    canvas.itemconfig(word, text=set_word, fill='black')
+    canvas.itemconfig(language, text='French', fill='black')
     canvas.itemconfig(card, image=card_front)
+    currently_french = True
+
+
+def change_to_english():
+    global currently_french
+    canvas.itemconfig(word, text=words_unknown[set_word], fill='white')
+    canvas.itemconfig(language, text='English', fill='white')
+    canvas.itemconfig(card, image=card_back)
+    currently_french = False
 
 
 def flip():
-    global set_word, French, timer
-    # window.after_id = None
+    global set_word, currently_french, timer
     restart_counter()
-    if French:
-        set_word = random.choice(list(words_unknown.keys()))
-        french()
-        French = False
-        # recount = window.after(3000, flip)
-        timer = window.after(3000, flip)
+    if currently_french:
+        print("Changing to english")
+        change_to_english()
     else:
-        canvas.itemconfig(word, text=words_unknown[set_word], fill='white')
-        canvas.itemconfig(language, text='English', fill='white')
-        canvas.itemconfig(card, image=card_back)
-        French = True
+        print("Changing to French")
+        change_to_french()
+        timer = window.after(3000, flip)
 
 
 def remove_known():
-    global set_word
-    if set_word in words_unknown:
-        print(f"removed {set_word}")
-        words_unknown.pop(set_word)
-        flip()
+    global currently_french, set_word, timer
+    print(currently_french)
+
+    if currently_french:
+        if set_word in words_unknown:
+            restart_counter()
+
+            print(f"removed {set_word} and {words_unknown[set_word]}")
+            words_unknown.pop(set_word)
+
+            df = pd.DataFrame.from_dict(words_unknown, orient='index')
+            df.to_csv('data/words_to_learn.csv')
+
+            change_to_french()
+
+            timer = window.after(3000, flip)
+
     else:
-        print(f"{set_word} NOT FOUND")
-        print(words_unknown)
-    # restart_counter()
+        if set_word in words_unknown:
+            print(f"removed {set_word} and {words_unknown[set_word]}")
+            words_unknown.pop(set_word)
 
+            df = pd.DataFrame.from_dict(words_unknown, orient='index')
+            df.to_csv('data/words_to_learn.csv')
 
-# current_word = ''
-#
-#
-# def display_english():
-#     global French, current_word
-#     French = False
-#     canvas.itemconfig(language, text='English')
-#     canvas.itemconfig(word, text=d[word])
-#
-#
-# def display_french():
-#     global French, current_word
-#     French = True
-#     canvas.itemconfig(language, text='French')
-#
-#
-# def get_word():
-#     global French, current_word
-#
-#     new_french_word = random.choice(list(d.keys()))
-#     canvas.itemconfig(word, text=new_french_word)
-#
-#     current_word = new_french_word
-#
-#     if French:
-#         display_english()
-#     else:
-#         display_french()
+            set_word = random.choice(list(words_unknown.keys()))
+            flip()
 
 
 # ------------------ UI ------------------------------------------------
@@ -113,10 +105,8 @@ word = canvas.create_text(400, 263, text='', font=("Ariel", 60, "bold"))
 
 try:
     words_unknown = pd.read_csv('data/words_to_learn.csv', index_col=0).squeeze('columns').to_dict()
-    set_word = random.choice(list(words_unknown.keys()))
-    print(set_word)
     print("Opened file")
-    french()
+    change_to_french()
     timer = window.after(3000, flip)
 except FileNotFoundError:
     print("File was not found... Creating file")
@@ -125,19 +115,7 @@ except FileNotFoundError:
     dataframe = pd.DataFrame.from_dict(words_unknown, orient="index")
     dataframe.to_csv('data/words_to_learn.csv')
     # --------------------------------------------- CREATING UNKNOWN WORDS FILE ------------------
-
-    print(f'List of the words unknown: ')
-    for key, item in words_unknown.items():
-        print(key, item)
-
-    # set_word = random.choice(list(unknown_words.keys()))
-    # words_unknown = unknown_words
-    set_word = random.choice(list(words_unknown.keys()))
-    canvas.itemconfig(word, text=set_word)
-    canvas.itemconfig(language, text='French')
-    canvas.itemconfig(card, image=card_front)
-    French = False
-    # recount = window.after(3000, flip)
+    change_to_french()
     timer = window.after(3000, flip)
 
 window.mainloop()
