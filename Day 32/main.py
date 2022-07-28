2e2d4f5ef7601761793e52cf154170b6dfc66a12
oscarsolis3301 <oscarsolis3301@gmail.com>
2022-07-27 21:43:27 -0600

    Finishing day 32

---
+++ Day 32/main.py
@@ -0,0 +1,84 @@
+import smtplib
+import datetime
+import random
+import pandas as pd
+
+EMAIL = "OscarLearnsPython@gmail.com"
+PASSWORD = "anklscijabewlidl"
+
+now = datetime.datetime.now()
+current_day = now.weekday()
+
+days = {0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday"}
+
+quotes_list = []
+
+current_day_of_week = days[current_day]
+random_quote = ''
+
+list_of_birthdays = pd.read_csv("list_of_birthdays.csv")
+df = pd.DataFrame(list_of_birthdays)
+birthdays = df.to_dict('records')
+
+BIRTHDAY_MONTH = 7
+BIRTHDAY_DAY = 27
+BIRTHDAY_YEAR = 2022
+
+birthday = []
+test_birthday = ''
+
+# Also, I know at one point or another you'll probably see this
+# So hiii beby <3
+# I love you so much :)
+
+for x in birthdays:
+    for y in x:
+
+        if type(x[y]) == int:
+            test_birthday += f"{x[y]}"
+            if len(test_birthday) == 7:
+                birthday.append(test_birthday)
+                test_birthday = ''
+        else:
+            pass
+
+birthday_amount = len(birthday)
+
+today = f'{now.year}{now.month}{now.day}'
+
+email_of_birthday_person_thing = ''
+name_of_birthday_person_thing = ''
+
+for x in birthday:
+    if x == today:
+        email_of_birthday_person_thing = birthdays[birthday.index(x)][' email'].strip()
+        name_of_birthday_person_thing = birthdays[birthday.index(x)]['name']
+
+        with open("quotes.txt") as quotes:
+            for quote in quotes:
+                quotes_list.append(quote)
+            random_quote = random.choice(quotes_list)
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=EMAIL, to_addrs=email_of_birthday_person_thing,
+                                msg=f"Subject:Happy Birthday {name_of_birthday_person_thing}\n\nToday is "
+                                    f"{current_day_of_week}, "
+                                    f" your birthday. Here's a quote"
+                                    f"for you: {random_quote}")
+
+            print(f'Email sent to {name_of_birthday_person_thing} since it is their birthday')
+
+# for year in now.year:
+#     for month in now.month:
+#         for day in now.month:
+#             for x in len(birthday):
+#                 if year == birthday[x]:
+#                     print('test')
