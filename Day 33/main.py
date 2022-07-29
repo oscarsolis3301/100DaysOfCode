824a4d96eaa3c32653b62112abf5b9a89330e4be
oscarsolis3301 <oscarsolis3301@gmail.com>
2022-07-28 18:25:58 -0600

    Working with Sunset API

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,12 +1,28 @@
 import requests
+#
+#
+# response = requests.get(url='http://api.open-notify.org/iss-now.json/')
+# response.raise_for_status()
+#
+# longitude = response.json()['iss_position']['longitude']
+# latitude = response.json()['iss_position']['latitude']
+#
+# iss_position = (longitude, latitude)
+#
+# params = {"lat": latitude,
+#           "lng": longitude}
+#
+# sunset = requests.get(url='https://api.sunrise-sunset.org/json/', parameters=params)
+# sunset.raise_for_status()
+#
+# print(iss_position)
 
+LATITUDE = 31.761877
+LONGITUDE = 106.485023
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json/')
+parameters = {"lat": LATITUDE,
+              "lng": LONGITUDE}
+response = requests.get(url='https://api.sunrise-sunset.org/json', verify=False, params=parameters)
 response.raise_for_status()
-
-longitude = response.json()['iss_position']['longitude']
-latitude = response.json()['iss_position']['latitude']
-
-iss_position = (longitude, latitude)
-
-print(iss_position)
+data = response.json()
+print(data)
