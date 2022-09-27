82f0be00c597a989d9e60c00dd6359b792405fba
oscar <oscarsolis3301@gmail.com>
2022-09-27 12:53:05 -0600

    Adding API key

--- Day 35/rain-alerts/main.py
+++ Day 35/rain-alerts/main.py
@@ -3,7 +3,7 @@ import requests
 param = {
     "lat": 33.685909,
     "lng": -117.824722,
-
+    "api_key": "69f04e4613056b159c2761a9d9e664d2"
 }
 
 response = requests.get(url='https://api.openweathermap.org/data/2.5/onecall?')
