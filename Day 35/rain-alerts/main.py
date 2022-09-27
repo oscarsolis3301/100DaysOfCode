d8e683ee1fe5b83c830dd343aebd9d92470b36a7
oscar <oscarsolis3301@gmail.com>
2022-09-27 12:54:36 -0600

    API

--- Day 35/rain-alerts/main.py
+++ Day 35/rain-alerts/main.py
@@ -3,7 +3,9 @@ import requests
 param = {
     "lat": 33.685909,
     "lng": -117.824722,
-    "api_key": "69f04e4613056b159c2761a9d9e664d2"
+    "appid": "69f04e4613056b159c2761a9d9e664d2",
+    "exclude": "current,minutely,daily"
 }
 
 response = requests.get(url='https://api.openweathermap.org/data/2.5/onecall?')
+
