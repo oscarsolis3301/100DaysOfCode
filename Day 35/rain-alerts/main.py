14f8497ece79ecdc04faa999233604836d4d7f71
Oscar <oscarsolis3301@gmail.com>
2022-11-16 09:59:42 -0700

    Checking if it's going to rain in the next 12 hours

--- Day 35/rain-alerts/main.py
+++ Day 35/rain-alerts/main.py
@@ -1,21 +1,21 @@
 import requests
+from twilio.rest import Client
 
-params = {
-    'lat': 31.761877,
-    'lon': -106.485023,
-    'appid': '69f04e4613056b159c2761a9d9e664d2'
-}
-response = requests.get(url='https://api.openweathermap.org/data/2.5/weather', verify=False, params=params)
-response.raise_for_status()
-print(response.status_code)
-data = response.json()
+# params = {
+#     'lat': 31.761877,
+#     'lon': -106.485023,
+#     'appid': '69f04e4613056b159c2761a9d9e664d2'
+# }
+# response = requests.get(url='https://api.openweathermap.org/data/2.5/weather', verify=False, params=params)
+# response.raise_for_status()
+# print(response.status_code)
+# data = response.json()
 
-print(data)
-import requests
+# print(data)
 
 param = {
-    "lat": 33.685909,
-    "lon": -117.824722,
+    "lat": 44.977753,
+    "lon": -93.265015,
     "appid": "69f04e4613056b159c2761a9d9e664d2",
     "exclude": "current,minutely,daily"
 }
@@ -23,12 +23,21 @@ param = {
 response = requests.get(url='https://api.openweathermap.org/data/2.5/onecall?', params=param)
 response.raise_for_status()
 weather = response.json()
-print(response.url)
-list_of_weather = (weather["hourly"][0:12])
-first_12_hours = []
-for x in list_of_weather:
-    first_12_hours.append(list_of_weather[0]['weather'][0]['id'])
+print("The url response is: " + response.url)
+will_rain = False
+list_of_weather = weather['hourly'][0:12]
+for hour in list_of_weather:
+    if hour['weather'][0]['id'] < 700:
+        will_rain = True
 
-for x in first_12_hours:
-    if x == 801:
-        print('')
+if will_rain:
+    print("It's going to rain today. Don't forget your umbrella")
+# list_of_all_weather = (weather["hourly"][:12])
+# print(list_of_all_weather[0])
+# first_12_hours = []
+# for x in list_of_weather:
+#     first_12_hours.append(list_of_weather[0]['weather'][0]['id'])
+#
+# for hours in first_12_hours:
+#     if hours < 700:
+#         print('')
