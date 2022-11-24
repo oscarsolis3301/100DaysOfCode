ca92240d4f3ff8dea121d2e8b36c8a43888608f7
Oscar <oscarsolis3301@gmail.com>
2022-11-23 21:52:15 -0700

    Finalizing messaging capabilities

--- Day 35/rain-alerts/main.py
+++ Day 35/rain-alerts/main.py
@@ -14,8 +14,8 @@ from twilio.rest import Client
 # print(data)
 
 param = {
-    "lat": 44.977753,
-    "lon": -93.265015,
+    "lat": 48.191311,
+    "lon": -122.126488,
     "appid": "69f04e4613056b159c2761a9d9e664d2",
     "exclude": "current,minutely,daily"
 }
@@ -30,8 +30,21 @@ for hour in list_of_weather:
     if hour['weather'][0]['id'] < 700:
         will_rain = True
 
+account_sid = "ACde37ac0c708218fe613b664bfc7f6003"
+auth_token = "df8a75478f0ca87b808e3510260f944f"
+
+client = Client(account_sid, auth_token)
+
 if will_rain:
-    print("It's going to rain today. Don't forget your umbrella")
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Don't forget your umbrella.",
+        from_='+13022087105',
+        to='+19498423049'
+    )
+    print(message.status)
+else:
+    print('It will not be raining in your area')
 # list_of_all_weather = (weather["hourly"][:12])
 # print(list_of_all_weather[0])
 # first_12_hours = []
