c659d541fbe76e3eee6ca66b62b45fe5cfbb9860
Oscar <oscarsolis3301@gmail.com>
2023-07-27 20:46:29 -0700

    Start of Day 5

---
+++ Day 5 - Password Generator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the PyPassword Generator")
+letters_amount = int(input("How many letters would you like in your password?"))
+symbols_amount = int(input("How many symbols would you like?"))
+numbers_amount = int(input("How many numbers would you like?"))
+total = letters_amount + symbols_amount + numbers_amount
+
