2a592c919dfc357ef3334a02688ebd46b2074d42
Oscar <oscarsolis3301@gmail.com>
2023-10-15 20:55:15 -0700

    Edit

--- Day 8 - Caesar Cipher/main.py
+++ Day 8 - Caesar Cipher/main.py
@@ -1,34 +1,43 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-running = True
-
-while (running):
-
-   def encrypt (text, shift):
-      end_result = ""
-      for letter in text:
-         if alphabet.index(letter) + shift < 25:
-            end_result += alphabet[alphabet.index(letter)+ shift]
-         else:
-            end_result += alphabet[shift % len(alphabet) - 1]
-      print(f"The new string is: {end_result}")
-
-
-   def decode (text, shift):
-      end_result = ""
-      for letter in text:
-         end_result += alphabet[alphabet.index(letter) - shift]
-      print(f"The new string is: {end_result}")
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+def encrypt(plain_text, shift_amount):
+    end_result = ""
+    for letter in plain_text:
+        if letter not in alphabet:
+            end_result += letter
+        else:
+            if alphabet.index(letter) + shift_amount < 25:
+                end_result += alphabet[alphabet.index(letter) + shift_amount]
+            else:
+                print("Index: " + str(shift_amount % len(alphabet)))
+                end_result += alphabet[(alphabet.index(letter) + shift_amount) % len(alphabet)]
+    print(f"The new string is: {end_result}")
 
 
-   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+def decode(plain_text, shift_amount):
+    end_result = ""
+    for letter in plain_text:
+        if letter not in alphabet:
+            end_result += letter
+        else:
+            end_result += alphabet[alphabet.index(letter) - shift_amount]
+    print(f"The new string is: {end_result}")
 
-   if direction == "encode":
-      text = input("Type your message:\n").lower()
-      shift = int(input("Type the shift number:\n"))
-      encrypt(text, shift)
-   elif direction == "decode":
-      text = input("Type your message:\n").lower()
-      shift = int(input("Type the shift number:\n"))
-      decode(text, shift)
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        decode(text, shift)
+    continue_running = input("Would you like to try again? (\"Yes\" or \"No\"")
+    if continue_running.lower() == "yes":
+        continue
+    elif continue_running.lower() == "no":
+        break
