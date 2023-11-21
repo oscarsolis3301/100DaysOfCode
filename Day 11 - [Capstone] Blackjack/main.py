9070f3344e2d1fa17ba280820e54e4e6894688a1
Oscar <oscarsolis3301@gmail.com>
2023-11-21 13:26:15 -0600

    Adding logic

--- Day 11 - [Capstone] Blackjack/main.py
+++ Day 11 - [Capstone] Blackjack/main.py
@@ -1,3 +1,7 @@
+import random
+
+from art import logo
+
 ############### Blackjack Project #####################
 
 # Difficulty Normal 😎: Use all Hints below to complete the project.
@@ -10,7 +14,7 @@
 ## The deck is unlimited in size.
 ## There are no jokers.
 ## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
+## The Ace can count as 11 or 1.
 ## Use the following list as the deck of cards:
 ## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 ## The cards in the list have equal probability of being drawn.
@@ -58,3 +62,99 @@
 # Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
 # Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+
+
+DECK = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+def check_winner(user_score, computer_score):
+    if 0 < user_score < 22 and 0 < computer_score < 22:
+        if user_score > computer_score:
+            print("You win!")
+        else:
+            print("You lost.")
+    elif computer_score > 21:
+        print("Opponent went over. You win!")
+
+
+while True:
+    user_deck = []
+    computer_deck = []
+    u_score = 0
+    c_score = 0
+
+    user_deck.append(random.choice(DECK))
+    user_deck.append(random.choice(DECK))
+
+    computer_deck.append(random.choice(DECK))
+    computer_deck.append(random.choice(DECK))
+
+    for cards in user_deck:
+        u_score += cards
+
+    for cards in computer_deck:
+        c_score += cards
+    if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'n':
+        print("Thank you for joining us.")
+        break
+    else:
+
+        while u_score < 22 and c_score < 22:
+            print(logo)
+            print(f"Your cards: {user_deck}, current score: {u_score}")
+            print(f"Computer's first card: {computer_deck[0]}")
+
+            if input("Type 'y' to get another card, type 'n' to pass: ").lower() != 'y':
+                check_winner(u_score, c_score)
+            else:
+                user_deck.append(random.choice(DECK))
+                computer_deck.append(random.choice(DECK))
+
+                u_score = sum(user_deck)
+                c_score = sum(computer_deck)
+        if u_score > 21:
+            print(f"You went over. You lose. Your score: {u_score} | Computer's score: {c_score}")
+        else:
+            print(f"The computer went over. You win! Your score: {u_score} | Computer's score: {c_score}")
+# print(logo)
+# user_total = 0
+# computer_total = 0
+# while user_total < 22 and computer_total < 22:
+#
+#     user_cards = []
+#     computer_cards = []
+#
+#     # Giving the user their deck of cards
+#     user_cards.append(random.choice(DECK))
+#     user_cards.append(random.choice(DECK))
+#
+#     for card in user_cards:
+#         user_total += card
+#
+#     print(f"\tYour cards: {user_cards}, current score: {user_total} ")
+#
+#     # Giving the computer their deck of cards
+#     computer_cards.append(random.choice(DECK))
+#     computer_cards.append(random.choice(DECK))
+#
+#     for card in computer_cards:
+#         computer_total += card
+#
+#     print(f"\tComputer's first card: {computer_cards[0]}")
+#
+#     keep_going = input("Type 'y' to get another card, type  'n' to pass: ").lower()
+#     if keep_going == 'n':
+#
+#         if 21 >= user_total > computer_total:
+#             print("You win!")
+#
+#     elif keep_going == 'y':
+#         user_cards.append(random.choice(DECK))
+#
+#         user_total = 0
+#         for card in user_cards:
+#             user_total += card
+#
+#         if user_total > 21:
+#             print("You went over. You busted.")
+#             break
